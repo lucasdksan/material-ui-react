f24f24b05ce8eb83090c62ff2812b6ad3067cbe6
Lucas <lucas.silva@agencian1.com.br>
2024-06-04 19:50:45 -0300

    feat: criação do environment

--- src/shared/services/axios/index.ts
+++ src/shared/services/axios/index.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { errorInterceptor, responseInterceptor } from "./tools";
+import { env } from "../../environment";
 
 const api = axios.create({
-    baseURL: "http://localhost:3333/"
+    baseURL: env.VITE_PORT
 });
 
 api.interceptors.response.use(
