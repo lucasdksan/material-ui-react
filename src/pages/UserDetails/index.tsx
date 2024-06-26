6e45dd247436353966f40e06c97ee7d68b1ffa6e
Lucas <lucas.silva@agencian1.com.br>
2024-06-26 13:24:24 -0300

    feat: adição da funcionalidade de deletar e algumas alterações basicas

--- src/pages/UserDetails/index.tsx
+++ src/pages/UserDetails/index.tsx
@@ -1,18 +1,57 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { LayoutDashBoard } from "../../shared/layouts";
 import { DetailTools } from "../../shared/components";
+import { useEffect, useState } from "react";
+import { usersServices } from "../../shared/services";
+import { LinearProgress } from "@mui/material";
 
 interface IUserDetailsProps { }
 
 export const UserDetails = ({}:IUserDetailsProps)=>{
     const { id = "new" } = useParams<"id">();
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
+    const [name, setName] = useState("");
 
-    const handleDelete = ()=> {}
+    useEffect(()=> {
+        if(id !== "new") {
+            setIsLoading(true);
+
+            usersServices.getById(parseInt(id))
+                .then((response)=>{
+                    setIsLoading(false);
+
+                    if(response instanceof Error) {
+                        alert(response.message);
+                        navigate("/users");
+                    } else {
+                        setName(response.name);
+                        console.log(response);
+                    }
+                });
+        }
+    }, []);
+
+    const handleDelete = ()=> {
+        if(confirm("Realmente deseja deletar?")) {
+            if(id !== "new") {
+                usersServices.deleteById(parseInt(id))
+                .then(result => {
+                    if(result instanceof Error) {
+                        alert(result.message);
+                    } else {
+                        alert("Usuário deletado com sucesso!");
+                        navigate("/users");
+                    }
+                });
+            }
+        }
+    }
+
     const handleSave = ()=> {}
 
     return(
-        <LayoutDashBoard title="Detalhe do Usuário"
+        <LayoutDashBoard title={id === "Nova" ? "Nova Pessoa": name}
             listingTools={
                 <DetailTools
                     textNewBtn="Novo"
@@ -27,6 +66,9 @@ export const UserDetails = ({}:IUserDetailsProps)=>{
                 />
             }
         >
+            { isLoading && (
+                <LinearProgress variant="indeterminate" />
+            ) }
             <div>Detalhe {id}</div>
         </LayoutDashBoard>
     );
